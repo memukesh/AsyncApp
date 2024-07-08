7b183e3f36441a1e3787c2e0951b3184ce60c8c7
magrawal000 <79099161+magrawal000@users.noreply.github.com>
2024-07-08 17:01:30 -0400

    name updated

--- AsyncApp/Program.cs
+++ AsyncApp/Program.cs
@@ -8,9 +8,9 @@ class Program
         Stopwatch sw = Stopwatch.StartNew();
         sw.Start();
         Console.WriteLine("Sending to Processors!");
-        var resultVisa = processVisa(new Reqeust { Amount = 1, CardNumber = "4234567890123456", CarName = "Visa name", CVV = 123, Expiry = "10/2026" });
-        var resultMC = processMC(new Reqeust { Amount = 2, CardNumber = "5234567890123456", CarName = "MC name", CVV = 123, Expiry = "11/2026" });
-        var resultAmex = processAmex(new Reqeust { Amount = 3, CardNumber = "323456789012345", CarName = "Amex name", CVV = 123, Expiry = "12/2026" });
+        var resultVisa = processVisaAsync(new Reqeust { Amount = 1, CardNumber = "4234567890123456", CarName = "Visa name", CVV = 123, Expiry = "10/2026" });
+        var resultMC = processMCAsync(new Reqeust { Amount = 2, CardNumber = "5234567890123456", CarName = "MC name", CVV = 123, Expiry = "11/2026" });
+        var resultAmex = processAmexAsync(new Reqeust { Amount = 3, CardNumber = "323456789012345", CarName = "Amex name", CVV = 123, Expiry = "12/2026" });
 
         var cardTasks = new List<Task> { resultVisa, resultMC, resultAmex };
         while (cardTasks.Count > 0)
@@ -36,19 +36,19 @@ class Program
         Console.WriteLine($"Time Elapsed {sw.ElapsedMilliseconds} miliseconds");
     }
 
-    public static  async Task<Result> processVisa(Reqeust reqeust)
+    public static  async Task<Result> processVisaAsync(Reqeust reqeust)
     {
         Console.WriteLine("sending it to Visa..");
          await Task.Delay(10000);
         return new Result { IsSuccess = true, ResponseID = 123, TransactionID = 123 };
     }
-    public static  async Task<Result> processMC(Reqeust reqeust)
+    public static  async Task<Result> processMCAsync(Reqeust reqeust)
     {
         Console.WriteLine("sending it to MC..");
          await Task.Delay(10000);
         return new Result { IsSuccess = true, ResponseID = 456, TransactionID = 456 };
     }
-    public static async Task<Result> processAmex(Reqeust reqeust)
+    public static async Task<Result> processAmexAsync(Reqeust reqeust)
     {
         Console.WriteLine("sending it to Amex..");
         await Task.Delay(10000);
