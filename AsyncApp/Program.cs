381665e70c853bbe5b4fb3f507cd4e8ef0bd9cc5
magrawal000 <79099161+magrawal000@users.noreply.github.com>
2024-07-08 16:48:48 -0400

    c

--- AsyncApp/Program.cs
+++ AsyncApp/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Runtime.CompilerServices;
 namespace AsyncApp;
 class Program
