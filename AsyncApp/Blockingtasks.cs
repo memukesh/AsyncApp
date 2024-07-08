b17b4966df4443c13d323e9a08512bdc526f083b
magrawal000 <79099161+magrawal000@users.noreply.github.com>
2024-07-08 16:48:01 -0400

    blocking code added

--- AsyncApp/Blockingtasks.cs
+++ AsyncApp/Blockingtasks.cs
@@ -2,9 +2,9 @@
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 namespace AsyncApp;
-class Program
+class Blockingtasks
 {
-    static async Task Main(string[] args)
+    static void Main1(string[] args)
     {
         Stopwatch sw = Stopwatch.StartNew();
         sw.Start();
@@ -13,46 +13,46 @@ class Program
         var resultMC = processMC(new Reqeust { Amount = 2, CardNumber = "5234567890123456", CarName = "MC name", CVV = 123, Expiry = "11/2026" });
         var resultAmex = processAmex(new Reqeust { Amount = 3, CardNumber = "323456789012345", CarName = "Amex name", CVV = 123, Expiry = "12/2026" });
 
-        var cardTasks = new List<Task> { resultVisa, resultMC, resultAmex };
-        while (cardTasks.Count > 0)
-        {
-            Task finishedTask = await Task.WhenAny(cardTasks);
-            if (finishedTask == resultVisa)
-            {
-                Console.WriteLine("Received response from Visa..");
-            }
-            else if (finishedTask == resultMC)
-            {
-                Console.WriteLine("Received response from MC..");
-            }
-            else if (finishedTask == resultAmex)
-            {
-                Console.WriteLine("Received response from Amex..");
-            }
-            await finishedTask;
-            cardTasks.Remove(finishedTask);
-        }
+        //var cardTasks = new List<Task> { resultVisa, resultMC, resultAmex };
+        //while (cardTasks.Count > 0)
+        //{
+        //    Task finishedTask = await Task.WhenAny(cardTasks);
+        //    if (finishedTask == resultVisa)
+        //    {
+        //        Console.WriteLine("Received response from Visa..");
+        //    }
+        //    else if (finishedTask == resultMC)
+        //    {
+        //        Console.WriteLine("Received response from MC..");            
+        //    }
+        //    else if (finishedTask == resultAmex)
+        //    {
+        //        Console.WriteLine("Received response from Amex..");
+        //    }
+        //    await finishedTask;
+        //    cardTasks.Remove(finishedTask);
+        //}
 
         sw.Stop();
         Console.WriteLine($"Time Elapsed {sw.ElapsedMilliseconds} miliseconds");
     }
 
-    public static  async Task<Result> processVisa(Reqeust reqeust)
+    public static  Result processVisa(Reqeust reqeust)
     {
         Console.WriteLine("sending it to Visa..");
-         await Task.Delay(10000);
+         Task.Delay(10000).Wait();
         return new Result { IsSuccess = true, ResponseID = 123, TransactionID = 123 };
     }
-    public static  async Task<Result> processMC(Reqeust reqeust)
+    public static  Result processMC(Reqeust reqeust)
     {
         Console.WriteLine("sending it to MC..");
-         await Task.Delay(10000);
+         Task.Delay(10000).Wait();
         return new Result { IsSuccess = true, ResponseID = 456, TransactionID = 456 };
     }
-    public static async Task<Result> processAmex(Reqeust reqeust)
+    public static Result processAmex(Reqeust reqeust)
     {
         Console.WriteLine("sending it to Amex..");
-        await Task.Delay(10000);
+        Task.Delay(10000).Wait();
         return new Result { IsSuccess = true, ResponseID = 987, TransactionID = 987 };
     }
     public class Reqeust
